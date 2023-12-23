aae7969140f1a55d00afc5d4146308c6b265e35d
Thiet Nguyen <nvthiet.it.95@gmail.com>
2023-12-23 11:22:20 +0700

    add CRUD function for account

--- accounts/src/main/java/com/xiaolin/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/xiaolin/accounts/entity/BaseEntity.java
@@ -5,7 +5,10 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -13,19 +16,19 @@ import java.time.LocalDateTime;
 @Getter @Setter @ToString
 public class BaseEntity {
 
-//    @CreatedDate
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-//    @CreatedBy
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
-//    @LastModifiedDate
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
-//    @LastModifiedBy
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 
